b495a480ebfcd0a66d5d90240163d582f93b5198
Jeffrey Arbelaez <Jeffreyarbelaez@yahoo.com>
2023-05-09 16:56:34 -0500

    completed assignemnt

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-// Code your solution in this file!
-const distanceFromHqInBlocks = (blocks) => {
-    return Math.abs(blocks - 42)
+function distanceFromHqInBlocks (block) {
+    return Math.abs(block - 42)
 }
 
-function distanceFromHqInFeet(someValue) {
-    const blocks = distanceFromHqInBlocks(someValue);
-    return blocks * 264
-    //const blocks = Math.abs(blocks-42)
+
+
+function distanceFromHqInFeet (blocks) {
+    return Math.abs((blocks -42) * 264)
 }
 
-const distanceTravelledInFeet = (pickup, dropoff) => {
-    const blocks = Math.abs(pickup - dropoff)
-    return blocks * 264
+
+function distanceTravelledInFeet (block1,block2) {
+    return Math.abs ((block1 - block2) *264 )
 }
 
-const calculatesFarePrice = (start, destination) => {
-    const feet = distanceTravelledInFeet(start, destination);
-    if(feet <= 400) {
-        return 0
-    } else if (feet >400 && feet < 2000) {
-        return (feet - 400) * .02
-    } else if (feet >=2000 && feet <= 2500) {
-        return 25
-    } else {
-        return 'cannot travel that far'
-    } 
+
+function calculatesFarePrice (start,destination) {
+    const distance = distanceTravelledInFeet(start,destination)
+    if (distance < 400) {
+    return (0)
+}   else if (distance >= 400 && distance <=2000) {
+    return ((distance - 400) * .02)
+}   else if (distance > 2000 && distance <=2500) {
+    return (25)
+}   else {
+    return ("cannot travel that far")
+}
 }
